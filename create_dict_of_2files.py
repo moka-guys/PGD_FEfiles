16c2276ada95355d095ae23f0c1b6ca1bda48526
aledj2 <aledjones@nhs.net>
2015-07-01 17:52:24 +0100

    with comments, ready to test

--- create_dict_of_2files.py
+++ create_dict_of_2files.py
@@ -9,18 +9,24 @@ import numpy
 import os
 
 class Merge_FEfile():
+    ''' Usage: python create_dict_of_2_files.py file1,file1_dye,file2,file2_dye 
+    File$ and File$_Dye specifies which samples (dye) you would like to combine
+    This program creates a new FE file from these two samples. 
+    NB The DLRS is recalculated but this calculation does not produce the same as what is produced during feature extraction 
+    The output file is names file1_file2.txt'''
+    
     #specify the input files
     chosenfolder="F:\\fefiles\\"#USB
 
     #name output file
     outputfolder="F:\\fefiles\\"#USB
     
-    
     # create dictionaries for features
     file1_dict={}
     file2_dict={}
 
     def get_sys_argvs(self,file1_in,dye1_in,file2_in,dye2_in):
+        '''capture the arguments'''
         # name global variables
         global file1
         global file2
@@ -31,14 +37,9 @@ class Merge_FEfile():
         file2=file2_in
         file1_dye=dye1_in
         file2_dye=dye2_in
-       
-#     def print_globals(self):
-#         print file1
-#         print file1_dye
-#         print file2
-#         print file2_dye
         
     def create_dicts (self):
+        '''open files, create the temporary file and add features to dictionaries '''
         #global variables
         global outputfile
         global outputfilename
@@ -54,7 +55,7 @@ class Merge_FEfile():
         tempoutputfilename="temp.txt"
         tempoutputfile=open(self.outputfolder+tempoutputfilename,'w')
         
-        # open the first file and write first 10 lines to the output file.
+        # open the first file and write first 10 lines (stats, feparams) to the output file.
         for i, line in enumerate (file1_open):
             if i < 10:
                 tempoutputfile.write(line)
@@ -74,12 +75,15 @@ class Merge_FEfile():
         file2_open.close()
     
     def rewrite_file(self):
-        # get the dye for each file
+        '''extract the desired sample(dye) from the dictionaries and re-write the fefile'''
+        
+        # get the specified dye for each file
         if str(file1_dye) in ("cy3","Cy3") and str(file2_dye) in ("cy3","Cy3"):
-            #then sort the dictionary on the keys so the file is in order of feature number
+            
+            #Sort the dictionary on the keys so the file is in order of feature number. remove newline
             for i in sorted(self.file1_dict.keys()):
                 file1_line=self.file1_dict[i].rstrip()
-                #for each feature split the line and assign to f1 
+                #for each feature split the line on tab and assign to f1 
                 f1=file1_line.split('\t')
                 #pull out the corresponding feature from file 2, split and assign to f2 
                 file2_line=self.file2_dict[i].rstrip()
@@ -87,15 +91,18 @@ class Merge_FEfile():
                 
                 #Variables used to concatenate the line to write to file
                 t="\t" # join
-                logratio=math.log(float(f2[13])/float(f1[13]),2) # calculate the log ratio
+                logratio=math.log(float(f2[13])/float(f1[13]),10) # calculate the log ratio
                 logratioerr=str(0)
                 pval=str(0)
-                #concatenate all the columns as required depending on the dyes selected
+                
+                #concatenate all the columns as required depending on the dyes selected. NB use of same f1 and f2 columns as the dyes are the same! 
                 to_write=f1[0]+t+f1[1]+t+f1[2]+t+f1[3]+t+f1[4]+t+f1[5]+t+f1[6]+t+f1[7]+t+f1[8]+t+f1[9]+t+str(logratio)+t+logratioerr+t+pval+t+f1[13]+t+f2[13]+t+f1[15]+t+f2[15]+t+f1[17]+t+f2[17]+t+f1[19]+t+f2[19]+t+f1[21]+t+f2[21]+t+f1[23]+t+f2[23]+t+f1[25]+t+f2[25]+t+f1[27]+t+f2[27]+t+f1[29]+t+f2[29]+t+f1[31]+t+f2[31]+t+f1[33]+t+f1[34]+t+f2[34]+t+f1[36]+t+f2[36]+t+f1[38]+t+f2[38]+t+f1[40]+t+f1[41]+t+f2[41]+"\n"
-                #print to_write
+                
+                #write this to the temporary output file
                 tempoutputfile.write(to_write)       
         
         # repeat for all combinations of dye      
+        
         if str(file1_dye) in ("cy3","Cy3") and str(file2_dye) in ("cy5","Cy5"):
             for i in sorted(self.file1_dict.keys()):
                 file1_line=self.file1_dict[i].rstrip()
@@ -104,12 +111,11 @@ class Merge_FEfile():
                 file2_line=self.file2_dict[i].rstrip()
                 f2=file2_line.split('\t')
                 t="\t"
-                print str(f1[1])+" "+str(f1[13])+" "+str(f2[14])
-                logratio=math.log(float(f2[14])/float(f1[13]),2)
+                logratio=math.log(float(f2[14])/float(f1[13]),10)
                 logratioerr=str(0)
                 pval=str(0)
+                # note different columns from f1+f2 selected as different dyes  
                 to_write=f1[0]+t+f1[1]+t+f1[2]+t+f1[3]+t+f1[4]+t+f1[5]+t+f1[6]+t+f1[7]+t+f1[8]+t+f1[9]+t+str(logratio)+t+logratioerr+t+pval+t+f1[13]+t+f2[14]+t+f1[15]+t+f2[16]+t+f1[17]+t+f2[18]+t+f1[19]+t+f2[20]+t+f1[21]+t+f2[22]+t+f1[23]+t+f2[24]+t+f1[25]+t+f2[26]+t+f1[27]+t+f2[28]+t+f1[29]+t+f2[30]+t+f1[31]+t+f2[32]+t+f1[33]+t+f1[34]+t+f2[35]+t+f1[36]+t+f2[37]+t+f1[38]+t+f2[39]+t+f1[40]+t+f1[41]+t+f2[42]+"\n"
-                #print to_write
                 tempoutputfile.write(to_write)
                    
         if str(file1_dye) in ("cy5","Cy5") and str(file2_dye) in ("cy3","Cy3"):
@@ -120,11 +126,10 @@ class Merge_FEfile():
                 file2_line=self.file2_dict[i].rstrip()
                 f2=file2_line.split('\t')
                 t="\t"
-                logratio=math.log(float(f2[13])/float(f1[14]),2)
+                logratio=math.log(float(f2[13])/float(f1[14]),10)
                 logratioerr=str(0)
                 pval=str(0)
                 to_write=f1[0]+t+f1[1]+t+f1[2]+t+f1[3]+t+f1[4]+t+f1[5]+t+f1[6]+t+f1[7]+t+f1[8]+t+f1[9]+t+str(logratio)+t+logratioerr+t+pval+t+f1[14]+t+f2[13]+t+f1[16]+t+f2[15]+t+f1[18]+t+f2[17]+t+f1[20]+t+f2[19]+t+f1[22]+t+f2[21]+t+f1[24]+t+f2[23]+t+f1[26]+t+f2[25]+t+f1[28]+t+f2[27]+t+f1[30]+t+f2[29]+t+f1[32]+t+f2[31]+t+f1[33]+t+f1[35]+t+f2[34]+t+f1[37]+t+f2[36]+t+f1[39]+t+f2[38]+t+f1[40]+t+f1[42]+t+f2[41]+"\n"
-                #print to_write
                 tempoutputfile.write(to_write)    
                 
         if file1_dye in ("cy5","Cy5") and file2_dye in ("cy5","Cy5"):
@@ -135,17 +140,15 @@ class Merge_FEfile():
                 file2_line=self.file2_dict[i].rstrip()
                 f2=file2_line.split('\t')
                 t="\t"
-                #print str(f1[13])+" "+str(f2[14])
-                
-                logratio=math.log(float(f2[14])/float(f1[14]),2)
+                logratio=math.log(float(f2[14])/float(f1[14]),10)
                 logratioerr=str(0)
                 pval=str(0)
                 to_write=f1[0]+t+f1[1]+t+f1[2]+t+f1[3]+t+f1[4]+t+f1[5]+t+f1[6]+t+f1[7]+t+f1[8]+t+f1[9]+t+str(logratio)+t+logratioerr+t+pval+t+f1[14]+t+f2[14]+t+f1[16]+t+f2[16]+t+f1[18]+t+f2[18]+t+f1[20]+t+f2[20]+t+f1[22]+t+f2[22]+t+f1[24]+t+f2[24]+t+f1[26]+t+f2[26]+t+f1[28]+t+f2[28]+t+f1[30]+t+f2[30]+t+f1[32]+t+f2[32]+t+f1[33]+t+f1[35]+t+f2[35]+t+f1[37]+t+f2[37]+t+f1[39]+t+f2[39]+t+f1[40]+t+f1[42]+t+f2[42]+"\n"
-                #print to_write
                 tempoutputfile.write(to_write)   
+        #close temp file now it's written
         tempoutputfile.close()
     
-    #some lists populate below 
+    #some lists that are populated below 
     array1=[] # holds chrom, start and log ratio from non control probes
     sortedarray=[] # array1 but sorted on genomic location
     log_dict={} # a dictionary with key = chrom, value = a list of log ratios for that chrom 
@@ -153,6 +156,15 @@ class Merge_FEfile():
     filtered_derivatives=[] # derivatives in the middle quartile
     
     def calculate_DLRS(self):
+        '''To calculate the DLRS the newly created temp file is read.
+        The chromosome, start and log ratio are added to a new array
+        This array is sorted on genomic coords.
+        Maintaining the order a list of log ratios for each chrom are added to a dictionary with chrom as the key.
+        For each chromosome the list is read and the previous log ratio is subtracted to create a list of derivatives (differences between probes) 
+        The interquartile ranges are calculated and only the middle 50% of derivatves are kept.
+        The SD of these derivatives is calculated
+        This is then put into the stats line and the final output file is created    
+        '''
         #open tempoutput file as read only
         outputfile2=open(self.outputfolder+tempoutputfilename,'r')
         for i, line in enumerate(outputfile2):
@@ -161,22 +173,28 @@ class Merge_FEfile():
                 splitline=line.split('\t')
                 # remove all control probes and check that there are the correct number of columns
                 if int(splitline[5])==0 and len(splitline)==43:
+                    
                     #split the genomic location into three new fields
                     genloc=splitline[7]
                     genloc=genloc.replace('chr','')
                     splitgenloc=genloc.replace('-',':').split(':')
+                    
                     #change X and Y to numerical
                     splitgenloc[0]=splitgenloc[0].replace('X','23')
                     splitgenloc[0]=splitgenloc[0].replace('Y','24')
+                    
+                    # insert the split genomic location into the line
                     splitline.insert(8,int(splitgenloc[0]))
                     splitline.insert(9,int(splitgenloc[1]))
                     splitline.insert(10,int(splitgenloc[2]))
+                    
                     #append to an array (chrom, start and logratio score)
                     self.array1.append((splitline[8],splitline[9],splitline[13]))
+                
                 else:
                     if int(splitline[5])==0:
                         #print any NON-control probes that do not have correct length
-                        print splitline
+                        print "error in line "+ splitline
             else:
                 pass        
         outputfile2.close()
@@ -191,32 +209,34 @@ class Merge_FEfile():
             list=[]
             for k in self.sortedarray:
                 if k[0]==i:
-                    print k
-                    list.append(k[2]) #### MAKE SURE THIS IS MAINTAINING ORDER
+                    #print k
+                    list.append(k[2]) 
             self.log_dict[i]=list
         
-        # for each chromosome
+        # self.log_dict is {(chrom: 'probe1 log score','probe 2 log score'...).(chrom2:'probe1 log score;,...)}
+        
+        # for each chromosome (i is the key (chrom))
         for i in self.log_dict:
             #how many probes are there
             num_of_probes=len(self.log_dict[i])
             # for that number of probes
             for j in range(num_of_probes):
-                #excluding the first one
+                #excluding the first probe (as there is no probe to subtract)
                 if j > 1:
                     #pull out log ratio and calculate the difference between that and the previous probe. subtract the previous one from this probe and append to a new list
-                    n=float(self.log_dict[i][j])-float(self.log_dict[i][j-1])###MAKE SURE THIS IS MAINTAINING THE ORDER
+                    n=float(self.log_dict[i][j])-float(self.log_dict[i][j-1])
                     self.all_derivatives.append(n)
         
-#         #use numpy to get the 25 and 75 percentile
-#         q75,q25=numpy.percentile(self.all_derivatives,[75,25])
-#         for i in self.all_derivatives:
-#             if q25< i <q75:
-#                 self.filtered_derivatives.append(i)
-#         #print "len of all_derivatives: "+str(len(self.all_derivatives))
-#         #print "len of filtered SD: "+str(len(self.filtered_derivatives))
+        #use numpy to get the 25 and 75 percentile
+        q75,q25=numpy.percentile(self.all_derivatives,[75,25])
+        for i in self.all_derivatives:
+            if q25< i <q75:
+                self.filtered_derivatives.append(i)
+        #print "len of all_derivatives: "+str(len(self.all_derivatives))
+        #print "len of filtered SD: "+str(len(self.filtered_derivatives))
         
         #calculate the DLSR by calculating the SD of this list    
-        DLSR=numpy.std(self.all_derivatives)
+        DLSR=numpy.std(self.filtered_derivatives)
         print "DLSR = "+str(DLSR)
          
         #open the final output file
@@ -243,9 +263,9 @@ class Merge_FEfile():
         outputfile3.close()
         finaloutput.close()
         
-        #os.remove(self.outputfolder+tempoutputfilename)
+        os.remove(self.outputfolder+tempoutputfilename)
         
-file1="example1.txt"
+file1="PGD_testfile1.txt"
 file2=file1
 file1_dye="cy3"
 file2_dye="cy5"
